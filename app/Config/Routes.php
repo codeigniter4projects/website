ba5c10261f8a5f6e0fe72b22ff63ffc463725c89
Lonnie Ezell <lonnieje@gmail.com>
2020-10-07 00:30:27 -0500

    Added Discuss page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,7 @@ $routes->setAutoRoute(false);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('discuss', 'Discuss::index');
 
 /**
  * --------------------------------------------------------------------
---
+++ app/Controllers/Discuss.php
@@ -0,0 +1,9 @@
+<?php namespace App\Controllers;
+
+class Discuss extends BaseController
+{
+    public function index()
+    {
+        echo $this->render('discuss');
+    }
+}
