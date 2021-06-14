34730aa0ff6459cfc9577f2a499ed571f43e5112
MGatner <mgatner@icloud.com>
2021-06-14 14:28:12 +0000

    Patch framework files to 4.1.3

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -39,9 +39,9 @@ class BaseController extends Controller
 	 */
 	protected $helpers = [];
 
-    /**
-     * @var GitHub
-     */
+	/**
+	 * @var GitHub
+	 */
 	protected $github;
 
 	/**
@@ -56,27 +56,27 @@ class BaseController extends Controller
 		// Do Not Edit This Line
 		parent::initController($request, $response, $logger);
 
-        $this->github = service('github');
+		$this->github = service('github');
 	}
 
-    /**
-     * Helper method to ensure we always have the info
-     * we need on every page.
-     *
-     * @param string $view
-     * @param array  $data
-     */
-    protected function render(string $view, array $data = [])
-    {
-        $this->response->noCache();
-        // Prevent some security threats, per Kevin
-        // Turn on IE8-IE9 XSS prevention tools
-        $this->response->setHeader('X-XSS-Protection', '1; mode=block');
-        // Don't allow any pages to be framed - Defends against CSRF
-        $this->response->setHeader('X-Frame-Options', 'DENY');
-        // prevent mime based attacks
-        $this->response->setHeader('X-Content-Type-Options', 'nosniff');
+	/**
+	 * Helper method to ensure we always have the info
+	 * we need on every page.
+	 *
+	 * @param string $view
+	 * @param array  $data
+	 */
+	protected function render(string $view, array $data = [])
+	{
+		$this->response->noCache();
+		// Prevent some security threats, per Kevin
+		// Turn on IE8-IE9 XSS prevention tools
+		$this->response->setHeader('X-XSS-Protection', '1; mode=block');
+		// Don't allow any pages to be framed - Defends against CSRF
+		$this->response->setHeader('X-Frame-Options', 'DENY');
+		// prevent mime based attacks
+		$this->response->setHeader('X-Content-Type-Options', 'nosniff');
 
-        return view($view, $data);
+		return view($view, $data);
 	}
 }
