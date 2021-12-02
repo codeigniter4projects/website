568fa877bef5817f2f43178b65d7492b7e3b3164
Arhell <arhell333@gmail.com>
2021-12-02 04:16:08 +0200

    add new favicons

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -5,7 +5,16 @@
     <title>Welcome to CodeIgniter</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('/favicons/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('/favicons/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('/favicons/favicon-16x16.png') ?>">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('/favicons/favicon.ico') ?>"/>
+    <link rel="manifest" href="<?= base_url('/favicons/site.webmanifest') ?>">
+    <link rel="mask-icon" href="<?= base_url('/favicons/safari-pinned-tab.svg') ?>" color="#ffffff">
+    <meta name="msapplication-config" content="<?= base_url('/favicons/browserconfig.xml') ?>" />
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
 
     <link rel="stylesheet" href="/assets/css/ci-theme.css">
     <link rel="stylesheet" href="/assets/css/ci-responsive.css?ver=1.0">
