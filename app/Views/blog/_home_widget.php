80cb5841729b5bf6b701d47fd788ecb8da2a9f83
Lonnie Ezell <lonnieje@gmail.com>
2021-12-15 22:12:14 -0600

    Fix tests

--- app/Views/blog/_home_widget.php
+++ app/Views/blog/_home_widget.php
@@ -1,12 +1,14 @@
 <?php if (isset($rows) && count($rows)) : ?>
     <?php foreach ($rows as $row) : ?>
-        <div class="rnapf-row">
-            <div class="rnapf-date">
-                <?= $row->formatDate() ?>
+        <?php if ($row instanceof \App\Entities\Post) : ?>
+            <div class="rnapf-row">
+                <div class="rnapf-date">
+                    <?= $row->formatDate() ?>
+                </div>
+                <div class="rnapf-title">
+                    <a href="<?= $row->link() ?>" class="rnapf-title-link"><?= esc($row->title) ?></a>
+                </div>
             </div>
-            <div class="rnapf-title">
-                <a href="<?= $row->link() ?>" class="rnapf-title-link"><?= esc($row->title) ?></a>
-            </div>
-        </div>
+        <?php endif ?>
     <?php endforeach ?>
 <?php endif ?>
