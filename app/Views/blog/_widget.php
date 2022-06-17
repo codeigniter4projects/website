198581f5118439fa3470556d7dfcaf6cc529019c
Lonnie Ezell <lonnieje@gmail.com>
2022-06-17 00:02:21 -0500

    Safety wrapper on widget

--- app/Views/blog/_widget.php
+++ app/Views/blog/_widget.php
@@ -2,8 +2,10 @@
     <h4><?= esc($title) ?></h4>
 
     <?php foreach ($rows as $row) : ?>
+        <?php if ($row !== null) : ?>
             <a href="<?= $row->link() ?>">
                 <?= esc($row->title) ?>
             </a>
+        <?php endif ?>
     <?php endforeach ?>
 </div>
