4f9afb634da6b3b343a2bf0419e645eb455bce9a
kenjis <kenji.uui@gmail.com>
2024-02-15 14:53:41 +0900

    Update "Contribute to CodeIgniter"

--- app/Views/contribute.php
+++ app/Views/contribute.php
@@ -7,7 +7,7 @@
     <div id="content-inner">
         <div id="contribute-heart-holder">
             <img src="/assets/icons/heart.png" id="contribute-heart" alt="heart icon"/>
-            <p>Contribute to CodeIgniter</p>
+            <p class="inner-page-text-box-title">Contribute to CodeIgniter</p>
         </div><!--heart ends here-->
 
         <div class="clr"></div>
@@ -20,12 +20,6 @@
                 on the <a href="https://github.com/codeigniter4/CodeIgniter4" target="_blank" class="link-primary">CodeIgniter4 repository</a> on GitHub.
             </p>
 
-            <div class="inner-page-text-box">
-                <p>
-                    The CodeIgniter 4 roadmap is explained on <a href="https://forum.codeigniter.com/forum-28.html" target="_blank" class="link-primary">our forum</a>.
-                </p>
-            </div>
-
             <p>
                 Issues are a quick way to point out a bug. If you find a bug or documentation error in CodeIgniter then please check
                 a few things first:
@@ -35,7 +29,7 @@
                 <li>There is not already an open Issue</li>
                 <li>The issue has already been fixed (check the develop branch, or look for closed Issues)</li>
                 <li>Is it something really obvious that you fix it yourself?</li>
-                <li>If you are unsure if you have found a bug, then start a new thread in the CodeIgniter forum, in the <a href="https://forum.codeigniter.com/forum-30.html" target="_blank" class="link-primary">Issues section</a>!</li>
+                <li>If you are unsure if you have found a bug, then start a new thread in the CodeIgniter forum, in the <a href="https://forum.codeigniter.com/forum-19.html" target="_blank" class="link-primary">Issues section</a>!</li>
             </ul>
             </p>
             <p>
@@ -47,9 +41,8 @@
         <div class="clr"></div>
 
         <div class="inner-page-text-box">
-            <div class="inner-page-text-box-title">CodeIgniter 3</div>
             <p>
-                CodeIgniter 3 has its own <a href="https://github.com/bcit-ci/CodeIgniter" target="_blank" class="link-primary">GiitHub repository</a>.
+                CodeIgniter 3 has its own <a href="https://github.com/bcit-ci/CodeIgniter" target="_blank" class="link-primary">GitHub repository</a>.
             </p>
         </div><!--contribute boxes ende-->
 
@@ -58,7 +51,7 @@
         <div class="warning">
             <p>
                 Security issues should be reported with an email to our security team, rather than being brought up on the forum or
-                raised as a GiitHub issue, thanks! Read more about responsible disclosure.
+                raised as a GitHub issue, thanks! Read more about responsible disclosure.
             </p>
         </div><!--warning ende-->
 
@@ -72,12 +65,9 @@
                 <div class="inner-page-text-sub-box-title">Testers</div>
                 <p>
                     We always need feedback on what works and what does not! Most of the development effort is going into Version 4,
-                    so that is where the need is greatest. If you find something that is definitely a bug, and you are a GiitHub user,
-                    please create a new "issue". If you are not a GiitHub user, or if you are unsure if you have found a bug, then start
-                    a new thread in the CodeIgniter forum <a href="https://forum.codeigniter.com/forum-30.html" target="_blank" class="link-primary">Issues section</a>!
-                </p>
-                <p>
-                    CodeIgniter 3 has its own <a href="https://forum.codeigniter.com/forum-19.html" target="_blank" class="link-primary boldy600">support subforum</a>.
+                    so that is where the need is greatest. If you find something that is definitely a bug, and you are a GitHub user,
+                    please create a new "issue". If you are not a GitHub user, or if you are unsure if you have found a bug, then start
+                    a new thread in the CodeIgniter forum <a href="https://forum.codeigniter.com/forum-19.html" target="_blank" class="link-primary">Issues section</a>!
                 </p>
             </div><!--inner-page-text-sub-box ende-->
 
@@ -86,7 +76,7 @@
             <div class="inner-page-text-sub-box">
                 <div class="inner-page-text-sub-box-title">Writers</div>
                 <p>
-                    Every project needs good documentation! The CodeIgniter user guide is part of the <a href="https://github.com/codeigniter4/CodeIgniter4" target="_blank" class="link-primary">GiitHub project</a> (mentioned above),
+                    Every project needs good documentation! The CodeIgniter user guide is part of the <a href="https://github.com/codeigniter4/CodeIgniter4" target="_blank" class="link-primary">GitHub project</a> (mentioned above),
                     and there is always room for more tutorials.
                 </p>
                 <p>
@@ -120,7 +110,7 @@
                 <div class="inner-page-text-sub-box-title">Designers</div>
                 <p>
                     Suggestions and help with our website, User Guide, and forum design are always welcome! We are working on themes
-                    for each of these, which will be shared in their own GiitHub repository.
+                    for each of these, which will be shared in their own GitHub repository.
                 </p>
             </div><!--inner-page-text-sub-box ende-->
 
@@ -135,14 +125,14 @@
             <div class="inner-page-text-sub-box">
                 <div class="inner-page-text-sub-box-title">Coders</div>
                 <p>
-                    If you would like to get involved in helping to build CodeIgniter4, join us on <a href="https://github.com/codeigniter4/" target="_blank" class="link-primary">GiitHub</a>!
+                    If you would like to get involved in helping to build CodeIgniter4, join us on <a href="https://github.com/codeigniter4/" target="_blank" class="link-primary">GitHub</a>!
                     A detailed contribution guide is in
                     <a href="https://github.com/codeigniter4/CodeIgniter4/blob/develop/contributing/README.md" target="_blank">Contributing to CodeIgniter</a>,
                     that it is properly documented, and that you use the
                     <a href="https://nvie.com/posts/a-successful-git-branching-model/" target="_blank" class="link-primary">Git-Flow branching model</a>.
                 </p>
                 <p>
-                    CodeIgniter 3 equivalent link: its <a href="https://github.com/bcit-ci/CodeIgniter/" target="_blank" class="link-primary boldy600">repository</a>.
+                    CodeIgniter 3 equivalent link: its <a href="https://github.com/bcit-ci/CodeIgniter/" target="_blank" class="link-primary">repository</a>.
                 </p>
             </div><!--inner-page-text-sub-box ende-->
 
