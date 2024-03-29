101d351e85ef7d008e4edf720efdcc8eac9f0e0c
kenjis <kenji.uui@gmail.com>
2024-03-29 19:14:17 +0900

    docs: fix PHPDoc types

--- app/Libraries/GitHub.php
+++ app/Libraries/GitHub.php
@@ -249,7 +249,7 @@ class GitHub
     /**
      * Retrieves repo details for each configured repository as a Repo.
      *
-     * @return list<Repo>
+     * @return array<string, Repo>
      */
     public function getRepos(): array
     {
