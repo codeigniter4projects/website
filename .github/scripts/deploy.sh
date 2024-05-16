cdcdc8e6ff750daa5b7c7c2a2e1790554a655f18
kenjis <kenji.uui@gmail.com>
2024-05-16 21:33:08 +0900

    chore: update deploy workflow

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -2,32 +2,52 @@
 
 # Deploys to the production server.
 
-RELEASE_DIR="/home/public_html/site/releases/"
-RSYNC_DIR="/home/public_html/site/latest/"
+REPO="/opt/website"
+RELEASE_DIR="/home/public_html/site/releases"
+SHARED_DIR="/home/public_html/site/shared"
+#RSYNC_DIR="/home/public_html/site/latest"
 USERGUIDE_DIR="/home/public_html/userguides"
 CONFIG_FILE="/home/public_html/config/.env.site"
 
-cd ~/
-RELEASE=`date +"%d-%m-%Y-%H-%M-%S"`
+RELEASE=`date +"%Y-%m-%d-%H-%M-%S"`
+
+echo $'Update website repository\n'
+cd $REPO
+git switch master
+git pull
 
 echo $'Copy current release\n'
 cd $RELEASE_DIR
-cp -r ../latest ./$RELEASE
+sudo cp -pr $REPO ./$RELEASE
 
 echo $'Install composer dependencies\n'
 cd $RELEASE_DIR/$RELEASE
 composer install --no-dev
 
-echo $'Setup FS\n'
-cd $RELEASE_DIR/$RELEASE
-sudo chmod -R 777 writable
-sudo chmod -R a+rx vendor
+if [ ! -d "$SHARED_DIR" ]; then
+    echo $'Create shared directory\n'
+    sudo mkdir -p "$SHARED_DIR"
+    echo $'Setup folder permissions\n'
+    sudo chown -R www-data:www-data writable
+    sudo chmod -R 755 writable
+    sudo cp -rp writable "$SHARED_DIR"
+fi
+
+echo $'Link writable\n'
+sudo rm -rf writable
+sudo ln -nsf "$SHARED_DIR/writable" writable
+
+echo $'Link .env\n'
 sudo ln -nsf $CONFIG_FILE .env
 
-echo $'Link current user guide\n'
-sudo ln -nsf $USERGUIDE_DIR/userguide4 public/user_guide
+echo $'Link user guides\n'
+ln -nsf $USERGUIDE_DIR/userguide4 public/user_guide
+ln -nsf $USERGUIDE_DIR/userguide3 public/userguide3
+ln -nsf $USERGUIDE_DIR/userguide2 public/userguide2
 
-echo $'Set up Links\n'
+echo $'Deploy: update symlink\n'
 cd $RELEASE_DIR
 sudo ln -nsf $RELEASE_DIR/$RELEASE "../current"
+
+echo $'Reload PHP8.1-FPM\n'
 sudo service php8.1-fpm reload
