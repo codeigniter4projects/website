f4ada02953c249db04c836becd44e5a78e7d740f
kenjis <kenji.uui@gmail.com>
2024-05-17 21:57:03 +0900

    chore: remove $ for echo
    `$'...'` is not supported by /bin/sh.

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -16,43 +16,43 @@ fi
 
 RELEASE=`date +"%Y-%m-%d-%H-%M-%S"`
 
-echo $'Update website repository\n'
+echo 'Update website repository\n'
 cd $REPO
 git switch master
 git pull
 
-echo $'Copy current release\n'
+echo 'Copy current release\n'
 cd $RELEASE_DIR
 sudo cp -pr $REPO ./$RELEASE
 
-echo $'Install composer dependencies\n'
+echo 'Install composer dependencies\n'
 cd $RELEASE_DIR/$RELEASE
 composer install --no-dev
 
 if [ ! -d "$SHARED_DIR" ]; then
-    echo $'Create shared directory\n'
+    echo 'Create shared directory\n'
     sudo mkdir -p "$SHARED_DIR"
-    echo $'Setup folder permissions\n'
+    echo 'Setup folder permissions\n'
     sudo chown -R www-data:www-data writable
     sudo chmod -R 755 writable
     sudo cp -rp writable "$SHARED_DIR"
 fi
 
-echo $'Link writable\n'
+echo 'Link writable\n'
 sudo rm -rf writable
 sudo ln -nsf "$SHARED_DIR/writable" writable
 
-echo $'Link .env\n'
+echo 'Link .env\n'
 sudo ln -nsf $CONFIG_FILE .env
 
-echo $'Link user guides\n'
+echo 'Link user guides\n'
 ln -nsf $USERGUIDE_DIR/userguide4 public/user_guide
 ln -nsf $USERGUIDE_DIR/userguide3 public/userguide3
 ln -nsf $USERGUIDE_DIR/userguide2 public/userguide2
 
-echo $'Deploy: update symlink\n'
+echo 'Deploy: update symlink\n'
 cd $RELEASE_DIR
 sudo ln -nsf $RELEASE_DIR/$RELEASE "../current"
 
-echo $'Reload PHP8.1-FPM\n'
+echo 'Reload PHP8.1-FPM\n'
 sudo service php8.1-fpm reload
